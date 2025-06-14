1de94d811a64a2bc00f3eacbf8a3ff9f08993eec
ulxsth <ulxsth@gmail.com>
2025-06-14 23:25:44 +0900

    chore: Nextjs おわり

--- docs/study/nextjs/nextjs-dashboard/app/ui/fonts.ts
+++
@@ -1,5 +0,0 @@
-import { Inter, Lusitana } from 'next/font/google';
-
-// subsets: 使用する文字だけを絞り込んで用いる
-export const inter = Inter({ subsets: ['latin'] });
-export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] });
